f6e1345e8172a66b8fc83fc3c626a9d4cca4e109
dlaumer <32751207+dlaumer@users.noreply.github.com>
2020-02-24 15:21:24 +0100

    First commit, file for reading Data

---
+++ ReadData3.py
@@ -0,0 +1,19 @@
+import json
+import pandas as pd
+from pandas.io.json import json_normalize  
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
+
+dataPath = '../Takeout/Location History/Location History.json'
+
+with open(dataPath) as f:
+    data = json.load(f)
+f.close()
+df = json_normalize(data, 'locations')
+df['datetime'] = pd.to_datetime(df['timestampMs'],  unit='ms')
+df = df.set_index('datetime')
+df.drop(['timestampMs'], axis=1, inplace=True)
+
+ax = df.plot.hist(bins=12)
