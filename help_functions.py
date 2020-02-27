7772b155e907173a7c430e5ae6312d1cf5a4662c
dlaumer <daniel.laumer@gmail.com>
2020-02-27 09:54:39 +0100

    New file structure (main, helper)

---
+++ help_functions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Help Functions
+    This file holds all the small helper functions or inventory functions.
+    
+    Created on Thu Feb 27 09:20:41 2020
+    Authors:    Daniel Laumer (laumerd@ethz.ch)
+                Haojun Cai (caihao@ethz.ch)
+    
+"""
+
+import json
+import pandas as pd
+from pandas.io.json import json_normalize  
+
+
+def parseLocs(dataPath):
+    with open(dataPath) as f:
+        data = json.load(f)
+    f.close()
+    df = json_normalize(data, 'locations')
+    df['datetime'] = pd.to_datetime(df['timestampMs'],  unit='ms')
+    df = df.set_index('datetime')
+    df.drop(['timestampMs'], axis=1, inplace=True)
+    return df
---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Main
+    This file is the main file, here all the main functions are called and 
+    the workflow is defined
+    
+    Created on Thu Feb 27 09:24:51 2020
+    Authors:    Daniel Laumer (laumerd@ethz.ch)
+                Haojun Cai (caihao@ethz.ch)
+    
+"""
+
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+
+# Local files
+import help_functions as hlp
+
+dataPathLocs = '../Takeout/Location History/Location History.json'
+
+locs = hlp.parseLocs(dataPathLocs)
+
