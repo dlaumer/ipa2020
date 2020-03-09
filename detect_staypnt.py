c05685654b64fcd219004a54d689a96360992f8a
CHJ2019 <haojuncai1996@gmail.com>
2020-03-09 16:17:31 +0100

    update haversine function

--- detect_staypnt.py
+++ detect_staypnt.py
@@ -12,18 +12,19 @@ import numpy as np
 from math import radians, cos, sin, asin, sqrt
 import matplotlib.pyplot as plt
 import plotly.express as px
+from haversine import haversine, Unit
 
 # Local files
 import help_functions as hlp
 
 # Read data
-# dataPathLocs = '../Takeout_Lauro_Mar/Standortverlauf/Standortverlauf.json'
-# dataPathTrips = '../Takeout_Lauro_Mar/Standortverlauf/Semantic Location History/'
+dataPathLocs = '../Takeout_Lauro_Mar/Standortverlauf/Standortverlauf.json'
+dataPathTrips = '../Takeout_Lauro_Mar/Standortverlauf/Semantic Location History/'
     
-dataPathLocs = '../Takeout_Haojun_Feb/Location History/Location History.json'
-locsdf, locsgdf = hlp.parseLocs(dataPathLocs)
+# dataPathLocs = '../Takeout_Haojun_Feb/Location History/Location History.json'
+# dataPathTrips = '../Takeout_Haojun_Feb/Location History/Semantic Location History/'
 
-dataPathTrips = '../Takeout_Haojun_Feb/Location History/Semantic Location History/'
+locsdf, locsgdf = hlp.parseLocs(dataPathLocs)
 trips, tripdf, tripgdf = hlp.parseTrips(dataPathTrips)
 
 #%%
@@ -36,15 +37,25 @@ lat1 = locsdf['latitudeE7'].iloc[:-1]
 lon1 = locsdf['longitudeE7'].iloc[:-1]
 lat2 = locsdf['latitudeE7'].iloc[1:]
 lon2 = locsdf['longitudeE7'].iloc[1:]
+
+# haver_vec = np.vectorize(hlp.haversine_built, otypes=[np.int16])
+# locsdf['d_diff'] = 0
+# locsdf['d_diff'].iloc[:-1] = (haver_vec(lat1,lon1,lat2,lon2))
+
+locsdf['d_diff_py'] = 0
+for i in range(0,len(locsdf)-1):
+    lat1 = locsdf['latitudeE7'].iloc[i]
+    lon1 = locsdf['longitudeE7'].iloc[i]
+    lat2 = locsdf['latitudeE7'].iloc[i+1]
+    lon2 = locsdf['longitudeE7'].iloc[i+1]
     
-haver_vec = np.vectorize(hlp.haversine, otypes=[np.int16])
-locsdf['d_diff'] = 0
-locsdf['d_diff'].iloc[:-1] = (haver_vec(lat1,lon1,lat2,lon2))
+    locsdf['d_diff_py'].iloc[i] = haversine((lat1,lon1), (lat2,lon2))*1000
 
 # drop last row
-locsdf.drop(locsdf.tail(1).index,inplace=True) 
+# locsdf.drop(locsdf.tail(1).index,inplace=True) 
 
 #%%
+# hist and set t_diff and d_diff thresholds
 fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
 axs[0].hist(locsdf['t_diff'])
 axs[1].hist(locsdf['d_diff'])
@@ -55,3 +66,8 @@ plt.show()
 # filter
 filter_tdiff = locsdf.loc[locsdf['t_diff']>30]
 stayPnt = filter_tdiff.loc[filter_tdiff['d_diff']<30]
+
+#%%
+# why negative values for distance
+selectRow = locsdf[locsdf['d_diff']<0]
+
