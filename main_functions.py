c19615e021a276223e63ace0a3ab253f7ade1f03
dlaumer <daniel.laumer@gmail.com>
2020-05-29 16:59:43 +0200

    More comments

--- main_functions.py
+++ main_functions.py
@@ -30,8 +30,22 @@ import trackintel_modified as tim
 import staypoints_modified as stm
 
 def findStayPoints(locs, dataName, accuracy_threshold, dist_threshold, time_threshold, timemax_threshold):
+    """
+    Finds the staypoints from the raw locations, following Li's algorithm with the help of trackintel   
+
+    Parameters
+    ----------
+    locs : gdf - raw points as a geopandas df
+    dataName : str - ID of participant
+    accuracy_threshold, dist_threshold, time_threshold, timemax_threshold : float - Different thresholds needed in the analysis
+
+    Returns
+    -------
+    pds : gdf - positionfixes, also the raw locations but in the format of trackintel
+    stps : gdf - staypoints, found by the algorithm of trackintel
+
+    """
     # Calculate time and distance difference
-    #if not(os.path.exists('../data/csv/'+dataName +'/' + dataName + '.csv')):
     if True:    
         locs['d_diff'] = np.append(haversine_dist(locs.longitudeE7[1:], locs.latitudeE7[1:], locs.longitudeE7[:-1], locs.latitudeE7[:-1]),0)
         
@@ -44,21 +58,51 @@ def findStayPoints(locs, dataName, accuracy_threshold, dist_threshold, time_thre
         hlp.loc2csv4ti(locs, dataName)
     pfs = ti.read_positionfixes_csv('../data/csv/'+dataName +'/' + dataName + '.csv', sep=';')
     
-    # Find staypoints
-    #stps = pfs.as_positionfixes.extract_staypoints(method='sliding',dist_threshold=100, time_threshold=5*60)
+    # Find staypoints using a slightly modified version of the trackintel script
     stps = tim.extract_staypoints_ipa(pfs, method='sliding',dist_threshold=dist_threshold,time_threshold=time_threshold,timemax_threshold=timemax_threshold)
     return pfs, stps
 
 def findPlaces(stps, dataName, minDist, minPoints):
-    
+    """
+    Used the result from findStayPoints() to cluster places with DBSCAN   
+
+    Parameters
+    ----------
+    stps : gdf - staypoints, found by the algorithm of trackintel
+    dataName: str - ID of participant
+    minDist, minPoints: float - Thresholds for the DBSCAN
+
+    Returns
+    -------
+    plcs : gdf - The clustered places defined by the DBSCAN
+
+    """
     # Find places
     plcs = stm.cluster_staypoints(stps,method='dbscan', epsilon=meters_to_decimal_degrees(minDist, 47.5), num_samples=minPoints)
     return plcs
 
 def findTrips(pfs, stps, plcs, dataName):
-    # Find the trips between the PLACES!!
+    """
+    Extract the trajectories between the found places   
+
+    Parameters
+    ----------
+    pds : gdf - positionfixes, also the raw locations but in the format of trackintel
+    stps : gdf - staypoints, found by the algorithm of trackintel
+    plcs : gdf - The clustered places defined by the DBSCAN
+    dataName: str - ID of participant
+
+    Returns
+    -------
+    tpls: gdf - triplegs, all trajectories between staypoints
+    trps: gdf - Trips, all trahectories between places
+    trpsCount: gdf - Only one trip per place pair, and geometry is a straight line
+
+    """
+    # First find the trips between the staypoints
     tpls = tim.extract_triplegs_ipa(pfs, stps)
     
+    # Prepare the gdf
     trps = pd.DataFrame(columns=['id', 'started_at', 'finished_at','start_plc', 'end_plc', 'geom'])
     trpsCount = pd.DataFrame(columns=['id','count', 'start_plc', 'end_plc', 'geom'])
 
@@ -66,16 +110,18 @@ def findTrips(pfs, stps, plcs, dataName):
     generated_trips_count = {}
 
     count = 0;
-    #countMatrix = np.zeros([len(plcs),len(plcs)])
     
     coordsCombined = []
     tplTemp = {}
+    # Go through each tripleg and combine them until a new place comes
     for i in range(len(tpls)):
         startPlace = stps.loc[tpls.loc[i,'start_stp'],'place_id']
         endPlace = stps.loc[tpls.loc[i,'end_stp'],'place_id']
         coords = tpls.loc[i,'geom'].coords[:]
+        # Case 1: tripleg is not between places, just add it
         if (startPlace == -1) and (endPlace == -1):
             coordsCombined = coordsCombined[:-1] + coords
+        # Case 2: tripleg starts at a place: start a new trip
         if (startPlace != -1):
             startCoord = plcs.loc[startPlace-1,'center'].coords[:]
             coordsCombined = startCoord + coords[1:]
@@ -87,7 +133,7 @@ def findTrips(pfs, stps, plcs, dataName):
                         'start_plc': startPlace,
                         'end_plc': None
                     }
-            
+        # Case 3: tripleg ends at a place: finish the entry
         if (endPlace != -1) and tplTemp:
             endCoord = plcs.loc[endPlace-1,'center'].coords[:]
             if (startPlace != -1):
@@ -99,7 +145,7 @@ def findTrips(pfs, stps, plcs, dataName):
             tplTemp["geom"] = LineString(coordsCombined)
             tplTemp["end_plc"] = endPlace
 
-            
+            # Here also prepare the entry for the counted trajectories
             ide = str(min(tplTemp["start_plc"], tplTemp["end_plc"])) + '_' + str(max(tplTemp["start_plc"], tplTemp["end_plc"]))
             #ide = str(startPlace) + '_' + str(endPlace)
             coords = [(coordsCombined[0]), (coordsCombined[-1])]
@@ -118,8 +164,6 @@ def findTrips(pfs, stps, plcs, dataName):
             else:
                 generated_trips_count[ide]['count'] = generated_trips_count[ide]['count']+ 1
                 generated_trips_count[ide]['trpIds'].append(count)
-            #countMatrix[startPlace-1,endPlace-1] = countMatrix[startPlace-1,endPlace-1] + 1
-            #countMatrix[endPlace-1,startPlace-1] = countMatrix[endPlace-1,startPlace-1] + 1
             count = count + 1
             coordsCombined = []
             tplTemp = {}
@@ -134,74 +178,33 @@ def findTrips(pfs, stps, plcs, dataName):
     return tpls, trps, trpsCount
 
 
-def findDirectTrips(pfs, stps, plcs, dataName):
-    # Find the trips between the PLACES!!
-    tpls = tim.extract_triplegs_ipa(pfs, stps)
-    
-    trps = pd.DataFrame(columns=['id', 'started_at', 'finished_at','start_plc', 'end_plc', 'geom'])
-    trpsAgr = pd.DataFrame(columns=['id','count', 'start_plc', 'end_plc', 'geom'])
-
-    generated_trips = []        
-    generated_trips_aggr = {}
-
-    count = 0;
-    #countMatrix = np.zeros([len(plcs),len(plcs)])
-    for i in range(len(tpls)):
-        startPlace = stps.loc[tpls.loc[i,'start_stp'],'place_id']
-        endPlace = stps.loc[tpls.loc[i,'end_stp'],'place_id']
-        if (startPlace != -1) and (endPlace!= -1):
-            coords = tpls.loc[i,'geom'].coords[1:-1]
-            startCoord = plcs.loc[startPlace-1,'center'].coords[:]
-            endCoord = plcs.loc[endPlace-1,'center'].coords[:]
-            coords = startCoord + coords + endCoord
-            
-            generated_trips.append({
-                        'id': count,
-                        'started_at': tpls.loc[i,'started_at'],  # pfs_tripleg['tracked_at'].iloc[0],
-                        'finished_at': tpls.loc[i,'finished_at'],  # pfs_tripleg['tracked_at'].iloc[-1],
-                        'geom': LineString(coords),
-                        'start_plc': startPlace,
-                        'end_plc': endPlace
-                    })
-            ide = str(min(startPlace,endPlace)) + '_' + str(max(startPlace,endPlace))
-            #ide = str(startPlace) + '_' + str(endPlace)
-            coords = startCoord + endCoord
-            if ide not in list(generated_trips_aggr):
-                generated_trips_aggr[ide] = {
-                        'id': ide,
-                        'count' : 1,
-                        'trpIds' : [count],
-                        'start_plc': startPlace,
-                        'end_plc': endPlace,
-                        'geom': LineString(coords),
-                    }
-            else:
-                generated_trips_aggr[ide]['count'] = generated_trips_aggr[ide]['count']+ 1
-                generated_trips_aggr[ide]['trpIds'].append(count)
-            #countMatrix[startPlace-1,endPlace-1] = countMatrix[startPlace-1,endPlace-1] + 1
-            #countMatrix[endPlace-1,startPlace-1] = countMatrix[endPlace-1,startPlace-1] + 1
-            
-            count = count + 1
-    
-    
-    trps = trps.append(generated_trips)
-    trps = gpd.GeoDataFrame(trps, geometry='geom')
-    
-    trpsAgr = trpsAgr.append(list(generated_trips_aggr.values()))
-    trpsAgr = gpd.GeoDataFrame(trpsAgr, geometry='geom')
-    
-    return tpls, trps, trpsAgr
-
-
 def clusterTrips(trps, trpsCount, minDistTh, factorTh, dataName, saveDendogramms = False):
+    """
+    Takes the trips from the previous steps and clusters and aggregates them. So in the end there are just a few trips per place pair   
+
+    Parameters
+    ----------
+    trps: gdf - Trips, all trahectories between places
+    trpsCount: gdf - Only one trip per place pair, and geometry is a straight line
+    minDistTh, factorTh: float - two thresholds needed for the clustering
+    dataName: str - ID of participant
+    saveDendogramms: boolean - Save all dendrograms to a folder
+
+    Returns
+    -------
+    trps: gdf - Trips, all trahectories between places, but added a column for the cluster ID
+    trpsAgr: gdf - Aggregated Trips 
+
+    """
     trps['length'] = trps['geom'].length
     trps['cluster'] = None
     
     trpsAgr = pd.DataFrame(columns=['weight', 'start_plc', 'end_plc', 'geom'])
     generated_trips_aggr = []
 
+    # Go through each place pair, that has connecting trips
     for i in trpsCount.index:
-    #for i in range(3,4):
+        # Find the places which it connects
         startPlace = trpsCount.loc[i,'start_plc']
         endPlace = trpsCount.loc[i,'end_plc']
         if startPlace == endPlace:
@@ -216,6 +219,7 @@ def clusterTrips(trps, trpsCount, minDistTh, factorTh, dataName, saveDendogramms
                         })
             continue
         
+        # Add all trajectories into another format (array of coordinates)
         trpsTemp = []
         for j in trpsCount.loc[i,'trpIds']:
             if trps.loc[j,'start_plc'] == endPlace:
@@ -223,19 +227,14 @@ def clusterTrips(trps, trpsCount, minDistTh, factorTh, dataName, saveDendogramms
                 trpsTemp.append(temp)
             else:
                 trpsTemp.append(trps.loc[j,'geom'].coords[:])
-        #trpsTemp = [trps.loc[j,'geom'].coords[:] for j in trpsCount.loc[i,'trpIds']]
+
+        # Make the distance matrix using DTW
         distMatrix = hlp.makeDistMatrix(trpsTemp)
-        #minIndices = np.where(distMatrix == np.nanmin(distMatrix))
-        #minIndices = list(zip(minIndices[0], minIndices[1]))
-        #minIndex = minIndices[0]
         
+        # Do the hierarchical clustering
         linkMatrix = linkage(distMatrix, method='complete')
-        
-        #a_coph = squareform(cophenet(linkMatrix))
-        #a_inco = inconsistent(linkMatrix)
-        #a_maxinc = maxinconsts(linkMatrix,a_inco)
-        #a_maxdist = maxdists(linkMatrix)
-        #a_maxRstat = maxRstat(linkMatrix, a_inco,0)
+
+        # Find the tree of the clustering result
         a_dn = dendrogram(linkMatrix)
         
         #Find outliers
@@ -251,19 +250,21 @@ def clusterTrips(trps, trpsCount, minDistTh, factorTh, dataName, saveDendogramms
                 if height2 > max(linkMatrix[:,2])/2 and linkMatrix[i,1] < len(trpsTemp):
                     outlierIdx.append(int(linkMatrix[i,1]))
         
+        # Remove outliers
         for idx in sorted(outlierIdx, reverse=True):
             del trpsTemp[idx]
         
         print(str(len(outlierIdx)) + " outliers were detected and removed")
         
+        # Do the clustering again, now without outliers
         distMatrix = hlp.makeDistMatrix(trpsTemp)   
         linkMatrix = linkage(distMatrix, method='complete')
         
-        
-        #tree = cut_tree(linkMatrix)
+        # Find the threshold on how to cut the tree
         th = max(minDistTh, max(linkMatrix[:,2])/factorTh)
         clusteringResult = fcluster(linkMatrix,th, 'distance')
-        #clusteringResult = tree[:,linkMatrix.shape[0] - 4]
+
+        # Maybe save the images of the trees
         if saveDendogramms:
             if not os.path.exists('../data/clustering/' + dataName + '/'):
                 os.mkdir('../data/clustering/' + dataName + '/')
@@ -272,30 +273,32 @@ def clusterTrips(trps, trpsCount, minDistTh, factorTh, dataName, saveDendogramms
             #plt.show()
             fig.savefig('../data/clustering/' + dataName + '/' + str(startPlace) + '_' + str(endPlace) + '_' +  str(max(clusteringResult)) + '.png')
         
-        #for idx, j in enumerate(trpsCount.loc[i,'trpIds']):
-        #    #for q in range(len(tree)):
-        #    trps.loc[j,'cluster'] = int(clusteringResult[idx])
-        
-        
-        
-        #Combining clusters 
+
+        # Aggregate the trips per cluster
         n = linkMatrix.shape[0] + 1
         numOfClusters = max(clusteringResult)
         clusters = defaultdict(dict)
+        # First add all original trajectories into one dict
         for idx, geom in zip(range(n), trpsTemp):
             clusters[idx]['geom'] = geom
             clusters[idx]['weight'] = 1
             
+        # Then go through them and combine them based on the info from the clustering step
+        # But only upto where the tree is cut (n-numOfClusters)
         for i in range(n-numOfClusters):
             cluster1 = linkMatrix[i,0]
             cluster2 = linkMatrix[i,1]
+            # Combine with weighted average
             newGeom = hlp.combineTrajectory(clusters[cluster1],clusters[cluster2])
             clusters[n + i] = {}
+            # Add new geometry
             clusters[n + i]['geom'] = newGeom
             clusters[n + i]['weight'] = clusters[cluster1]['weight'] + clusters[cluster2]['weight']
+            # Remove original trips
             clusters.pop(cluster1)
             clusters.pop(cluster2)
         
+        # Then in the end add the remaining aggregated trips (often just 1-3) to the gdf
         for idx, cluster in enumerate(clusters):
             ide = str(min(startPlace,endPlace)) + '_' + str(max(startPlace,endPlace)) + '_' + str(idx)
             generated_trips_aggr.append({
